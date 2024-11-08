077e0b241390b3514e1c8473f0c5edce12cc4c8b
VivekParida <tejaswivivek23@gmail.com>
2024-11-09 01:34:10 +0530

    Learning React

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function Message(){
+    //JSX: JavaScript XML
+    const name = "Vivek";
+    if(name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello Guest</h1>
+}
+
+export default Message; //exporting the component
